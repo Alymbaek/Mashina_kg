e11985549fcc3d4534ee70d77d6de4581c3840ab
Alymbaek <alymbekibragimov46@gmail.com>
2024-10-17 14:06:22 +0600

    new cods

--- mashina_site/store/models.py
+++ mashina_site/store/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Marka(models.Model):
+    marka_name = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.marka_name
+
+class Model(models.Model):
+    marka = models.ForeignKey(Marka, related_name='model', on_delete=models.CASCADE)
+    model_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.model_name
+
+class Car(models.Model):
+    marka = models.ForeignKey(Marka, on_delete=models.CASCADE)
+    model = models.ForeignKey(Model, on_delete=models.CASCADE)
+    year_of_release = models.DateField()
+    mileage = models.PositiveSmallIntegerField(default=0, null=True, blank=True)
+    the_body = models.CharField(max_length=100, null=True, blank=True)
+    colour = models.CharField(max_length=100, null=True, blank=True)
+    engine = models.CharField(max_length=100, null=True, blank=True)
+    BOX_CHOICES = [
+        ('Механик', 'Механик'),
+        ('Автомат', 'Автомат'),
+    ]
+    box = models.CharField(max_length=100, choices=BOX_CHOICES)
+    DRIVE_CHOICES = [
+        ('полный', 'Полный'),
+        ('задний', 'Задний'),
+        ('Передний', 'Передний')
+    ]
+    drive = models.CharField(max_length=100, choices=DRIVE_CHOICES)
+    RUDDER_CHOICES = [
+        ('Левый', 'Левый'),
+        ('Правый', 'Правый'),
+    ]
+    rudder = models.CharField(max_length=10)
+
+
+
+
+
